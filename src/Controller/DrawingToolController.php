b487f853502604e94391dc951a93ae6d45b05a65
Angel Gonzalez <angeledugo@gmail.com>
2024-07-13 15:28:41 -0500

    Carga inicial controller Drawing y servicio

---
+++ src/Controller/DrawingToolController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DrawingToolController extends AbstractController
+{
+    
+    #[Route('/', name: 'drawing_tool', methods: ['GET', 'POST'])]
+    public function index(): Response
+    {
+        $result = '';
+
+        if ($request->isMethod('POST')) {
+            $input = $request->request->get('commands');
+            $result = $drawingTool->executeCommands($input);
+        }
+        return $this->render('drawing_tool/index.html.twig', [
+            'controller_name' => 'DrawingToolController',
+            'result' => $result
+        ]);
+    }
+}
---
+++ src/services/DrawingTool.php
@@ -0,0 +1,28 @@
+<?php 
+namespace App\Services;
+
+class DrawingTool {
+
+    // Array Bidemensional Lienzo 
+    private array $canvas = [];
+
+
+
+    /**
+     * Recibe un string con todos los comandos y separa por lineas 
+     * renderiza el lienzo como una cadena
+     */
+    public function executeCommands(string $input) {
+                    
+        
+    }
+
+    /**
+     * Toma una Cadena con un comando individual y llama 
+     * a su funcion basadose en la primera letra
+     */
+
+    public function executeCommand() {
+
+    }
+}
